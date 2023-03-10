f1e9634e8c9787b36d3e2dc8ebc669e29959bee0
LahFlame <onuhjorshh@gmail.com>
2023-03-10 17:47:11 +0100

    modified the calculator seperate numbers in threes with commas and improved responsiveness

--- src/components/button-screen/button-screen.component.jsx
+++ src/components/button-screen/button-screen.component.jsx
@@ -2,10 +2,18 @@ import React from "react"
 import "./button-screen.styles.scss"
 
 const ButtonScreen = ({result,input,displayResult,displayNum}) => {
+    const numberWithCommas = (x) => {
+        return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+    }
     return(
         <div className="button-screen">
             <span className={`default ${displayResult ? null : displayNum === false || displayNum.length === 0  ? "animate" : null} `}>0</span>
-            <span className={`${displayResult? "small" : null}`}>{displayResult ? result : displayNum ? input : null}</span>
+            <span className={`${displayResult? "small" : null}`}>
+                {
+                    
+                    displayResult && (result.toString().length > 21) ? result : displayResult && (result.toString().length < 22) ? numberWithCommas(result) : displayNum ? input : null
+                }
+            </span>
         </div>
     )
 }
